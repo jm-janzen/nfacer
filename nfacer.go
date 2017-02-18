7e2c7959200c129ef72830cb3890ec821742c4cd
JM <jmjanzen@gmail.com>
2017-02-18 11:56:26 -0500

    Revert "Converts tabs -> spaces(4)"
    Oops!  Turns out tabs is the convention - will try to learn
    to live with it. :/
    
    This reverts commit 5cca746190975a6a587620fe298a903b76116bba.

--- nfacer.go
+++ nfacer.go
@@ -1,54 +1,54 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "strings"
+	"log"
+	"net/http"
+	"strings"
 
-    common "./utils"         // My common helper functions
+	common "./utils"         // My common helper functions
 
-    "github.com/gorilla/mux" // HTTP request  router
-    "github.com/yosssi/ace"  // HTML template engine
+	"github.com/gorilla/mux" // HTTP request  router
+	"github.com/yosssi/ace"  // HTML template engine
 )
 
 // Handlers receive and log an HTTP req, then serve our pages (using _render)
 func HandleHome(w http.ResponseWriter, r *http.Request) {
-    render(w, r, "base")
+	render(w, r, "base")
 }
 
 func HandleOther(w http.ResponseWriter, r *http.Request) {
-    render(w, r, "other")
+	render(w, r, "other")
 }
 
 func render(w http.ResponseWriter, r *http.Request, template string) {
-    var requestedPath = r.URL.Path[1:] // Trim leading `/'
+	var requestedPath = r.URL.Path[1:] // Trim leading `/'
 
-    // Print IP (sans port), requested path
-    log.Println(strings.Split(r.RemoteAddr, ":")[0], requestedPath)
+	// Print IP (sans port), requested path
+	log.Println(strings.Split(r.RemoteAddr, ":")[0], requestedPath)
 
-    // Load given template by name
-    tpl, err := ace.Load("templates/"+template, "", nil)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
+	// Load given template by name
+	tpl, err := ace.Load("templates/"+template, "", nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-    // Apply parsed template to w
-    if err := tpl.Execute(w, nil); err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
+	// Apply parsed template to w
+	if err := tpl.Execute(w, nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
 func main() {
-    // Change dir to project root, if not already there
-    common.ChdirWebserverRoot()
+	// Change dir to project root, if not already there
+	common.ChdirWebserverRoot()
 
-    r := mux.NewRouter()
+	r := mux.NewRouter()
 
-    r.HandleFunc("/", HandleHome)
-    r.HandleFunc("/other", HandleOther)
+	r.HandleFunc("/", HandleHome)
+	r.HandleFunc("/other", HandleOther)
 
-    http.Handle("/", r)
-    http.ListenAndServe(":8080", nil)
+	http.Handle("/", r)
+	http.ListenAndServe(":8080", nil)
 }
